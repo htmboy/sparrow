9c68e178741d2135552dfe681e25b968a711c54b
htmboy <htmboy8@msn.com>
2020-07-31 22:39:34 +0800

    显示个人资料

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class UserRequest extends FormRequest
 {
@@ -24,7 +25,7 @@ class UserRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
+            'name' => 'required|between:5,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:sparrow_users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
         ];
@@ -35,7 +36,7 @@ class UserRequest extends FormRequest
         return [
             'name.unique' => '用户名已被占用，请重新填写',
             'name.regex' => '用户名只支持英文、数字、横杠和下划线。',
-            'name.between' => '用户名必须介于 3 - 25 个字符之间。',
+            'name.between' => '用户名必须介于 5 - 25 个字符之间。',
             'name.required' => '用户名不能为空。',
         ];
     }
